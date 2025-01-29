01082b9790a3d852677c24c9fe15d3d9692dc2bd
Rajan <mail.rajansinghal@gmail.com>
2025-01-30 04:12:39 +0530

    updated the add method to look for the custom  delimiter at the beginning in the input string.

--- string_calculator.py
+++ string_calculator.py
@@ -1,10 +1,15 @@
 def add(numbers: str) -> int:
     if numbers == "":
         return 0
+    if numbers.startswith("//"):
+        delimiter_end_index = numbers.index("\n") 
+        delimiter = numbers[2:delimiter_end_index]
+        numbers = numbers[delimiter_end_index + 1:]
+        numbers = numbers.replace(delimiter, ",")
     numbers = numbers.replace("\n", ",")
     '''
     Split the string by commas, convert each value to an integer, 
     and sum them up
     '''
-    numbers_list = map(int,numbers.split(',')) 
+    numbers_list = map(int, numbers.split(','))
     return sum(numbers_list)
