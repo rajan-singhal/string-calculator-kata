68feeea9c8a6afed246f26fae56c7c18ab4b9676
Rajan <mail.rajansinghal@gmail.com>
2025-01-30 03:24:24 +0530

    updated the add method in the string_calculator.py file to handle the addition of one or two numbers:

--- string_calculator.py
+++ string_calculator.py
@@ -1,4 +1,9 @@
 def add(numbers: str) -> int:
     if numbers == "":
         return 0
-    return int(numbers)
+    '''
+    Split the string by commas, convert each value to an integer, 
+    and sum them up
+    '''
+    numbers_list = map(int,numbers.split(',')) 
+    return sum(numbers_list)
