d0b86a87c45cc7a0d7e1345eaf4952efe2e14f76
Rajan <mail.rajansinghal@gmail.com>
2025-01-30 04:34:00 +0530

    modified the add method to check for negative numbers and raise a ValueError with the appropriate message.

--- string_calculator.py
+++ string_calculator.py
@@ -2,14 +2,22 @@ def add(numbers: str) -> int:
     if numbers == "":
         return 0
     if numbers.startswith("//"):
-        delimiter_end_index = numbers.index("\n") 
+        delimiter_end_index = numbers.index("\n")
         delimiter = numbers[2:delimiter_end_index]
         numbers = numbers[delimiter_end_index + 1:]
         numbers = numbers.replace(delimiter, ",")
     numbers = numbers.replace("\n", ",")
-    '''
-    Split the string by commas, convert each value to an integer, 
-    and sum them up
-    '''
-    numbers_list = map(int, numbers.split(','))
-    return sum(numbers_list)
+
+    numbers_list = numbers.split(",")
+    negatives = []
+
+    for num in numbers_list:
+        number = int(num)
+        if number < 0:
+            negatives.append(number)
+
+    if negatives:
+        raise ValueError(
+            f"Negative numbers are not allowed: {', '.join(map(str, negatives))}")
+
+    return sum(map(int, numbers_list))
