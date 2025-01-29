77a793c734693254342a18d5b193e44286bda1f1
Rajan <mail.rajansinghal@gmail.com>
2025-01-30 02:59:04 +0530

    Implemented test case for single value string

--- test_string_calculator.py
+++ test_string_calculator.py
@@ -3,3 +3,7 @@ from string_calculator import add
 def test_empty_string():
 	"""it should return 0 if the string is empty"""
 	assert add('') == 0
+
+def test_single_value_string():
+	"""it should return value itself if the string is single value"""
+	assert add('1') == 1
