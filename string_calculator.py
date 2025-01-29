12209c1f293753994e49b44723536c53b4d68802
Rajan <mail.rajansinghal@gmail.com>
2025-01-30 05:17:25 +0530

    modified the add() method to handle multiple delimiters of any length

--- string_calculator.py
+++ string_calculator.py
@@ -1,15 +1,20 @@
+import re
+
+
 def add(numbers: str) -> int:
     if numbers == "":
         return 0
     if numbers.startswith("//"):
         delimiter_end_index = numbers.index("\n")
-        delimiter = numbers[2:delimiter_end_index]
+        delimiter_section = numbers[2:delimiter_end_index]
 
-        if delimiter.startswith("[") and delimiter.endswith("]"):
-            delimiter = delimiter[1:-1]
-
+        if delimiter_section.startswith("["):
+            delimiters = re.findall(r'\[([^\]]+)\]', delimiter_section)
+        else:
+            delimiters = [delimiter_section]
         numbers = numbers[delimiter_end_index + 1:]
-        numbers = numbers.replace(delimiter, ",")
+        for delimiter in delimiters:
+            numbers = numbers.replace(delimiter, ",")
     numbers = numbers.replace("\n", ",")
 
     numbers_list = numbers.split(",")
